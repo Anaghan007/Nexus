5be75b03830d3a8ff8dc438508aec6f9cf51a6f8
Anaghan007 <106956239+Anaghan007@users.noreply.github.com>
2026-05-21 23:44:39 +0530

    index

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'nexus-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/Logo.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch events (Network falling back to cache)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
